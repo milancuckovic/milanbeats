8e8cbe607bb9224717d8815033da3075cffd8c8a
milancuckovic <milancuckovic94@gmail.com>
2022-02-03 16:14:01 +0100

    Updates

--- src/Components/Moon.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Moon() {
-  return <img src="./Images/moon.png" className="moon"/>;
-}
--- src/Page/About.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Footer from '../Components/Footer';
-import HeaderAbout from '../Components/HeaderAbout';
-export default function About() {
-  return <>
-  <HeaderAbout/>
-  <Footer/>
-</>;
-}
